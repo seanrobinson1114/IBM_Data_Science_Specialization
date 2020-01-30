a73aa50ba09c35cba7afcff14c7d28fd24af8560
seanrobinson1114 <seanrobinson1114@gmail.com>
2020-01-30 10:25:22 -0600

    minor changes to plotting

--- survery_results_bar_chart.py
+++ survery_results_bar_chart.py
@@ -19,18 +19,26 @@ survey_results_sorted_percentage_df = survey_results_sorted_df.div(2233.00).roun
 print(survey_results_sorted_percentage_df)
 
 # create bar chart
-survey_results_sorted_percentage_df.plot(
-    kind="bar", 
-    figsize=(20,8), 
-    width=0.8, 
-    fontsize=14)
+# survey_results_sorted_percentage_df.plot(
+#     kind="bar", 
+#     figsize=(20,8), 
+#     width=0.8, 
+#     fontsize=14,
+#     color=("#5cb85c","5bc0de", "#d9534f")
+# )
+
+fig = plt.figure(figsize=(20,8))
+ax = fig.add_subplot(111)
 
 plt.title(label="Percentage of Respondents' Interest in Data Science Areas", fontsize=16)
-plt.legend(fontsize=14)
-plt.gca().spines['right'].set_color('none')
-plt.gca().spines['left'].set_color('none')
-plt.gca().spines['bottom'].set_color('none')
-plt.gca().spines['top'].set_color('none')
+#plt.legend(fontsize=14)
+# plt.gca().spines['right'].set_color('none')
+# plt.gca().spines['left'].set_color('none')
+# plt.gca().spines['bottom'].set_color('none')
+# plt.gca().spines['top'].set_color('none')
 
-#plt.title("Percentage of Respondents' Interest in Data Science Areas")
-plt.show()
+survey_results_sorted_percentage_df["Very interested"].plot(kind="bar", color="#5cb85c", width=0.8, position=1)
+survey_results_sorted_percentage_df["Somewhat interested"].plot(kind="bar", color="#5bc0de", width=0.8, position=0)
+survey_results_sorted_percentage_df["Not interested"].plot(kind="bar", color="#d9534f", width=0.8, position=-1)
+
+plt.show() 
